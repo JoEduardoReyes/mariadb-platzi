3786d3816aec5e3ffc46adb02adf1b15102fda23
Eduardo Reyes <joeduardoreyes@outlook.com>
2024-05-07 17:55:32 -0600

    Borrando datos: DELETE

---
+++ 10-delete-from.sql
@@ -0,0 +1,13 @@
+DELETE FROM `stations_delete`;
+-- Esto es para que vean como un delete from no establece los ids
+INSERT INTO `stations_delete` (name)
+VALUES ("Lázaro Cárdens"),
+  -- Lázaro Cárdenas
+  ("Ferería"),
+  -- Ferrería 
+  ("Pnttlán"),
+  -- Pantitlán
+  ("Tauga"),
+  -- Tacuba
+  ("MartínCrrera");
+-- Martín Carrera
