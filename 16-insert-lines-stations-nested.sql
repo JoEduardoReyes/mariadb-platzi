889c502d3afbfc2325a7f0fae7a42eb830894c31
Eduardo Reyes <joeduardoreyes@outlook.com>
2024-05-13 18:19:35 -0600

    Creando nuestra primer consulta anidada

---
+++ 16-insert-lines-stations-nested.sql
@@ -0,0 +1,13 @@
+INSERT INTO `lines_stations` (line_id, station_id)
+VALUES (
+    (
+      SELECT `lines`.`id`
+      FROM `lines`
+      WHERE `lines`.`name` = "Línea 9"
+    ),
+    (
+      SELECT `stations`.`id`
+      FROM `stations`
+      WHERE `stations`.`name` = "Lázaro Cárdenas"
+    )
+  )
